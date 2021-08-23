f457f7693113c783dece6eebaf4f58eebd4e8e59
jefaokpta <jefaokpta@hotmail.com>
2021-08-23 15:01:01 -0300

    core principal do whats finalizado

--- src/static/staticVars.js
+++ src/static/staticVars.js
@@ -2,5 +2,5 @@
 
 export const urlBase = 'http://127.0.0.1:8080'
 export const mediaFolder = 'whatsMedia'
-// const urlBase = 'http://128.0.0.1:8080'
-// const mediaFolder = '/whatsMedia'
+// export const urlBase = 'http://128.0.0.1:8080'
+// export const mediaFolder = '/whatsMedia'
