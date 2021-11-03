eb882fb16b2a650666371800236f37faf915ab45
jefaokpta <jefaokpta@hotmail.com>
2021-11-03 14:38:54 -0300

    add ambiente homolog

--- src/static/staticVars.js
+++ src/static/staticVars.js
@@ -1,6 +1,6 @@
 
-
 export const urlBase = 'http://127.0.0.1:8080'
 export const mediaFolder = 'whatsMedia'
+// export const urlBase = 'http://128.0.0.1:9090'
 // export const urlBase = 'http://128.0.0.1:8080'
 // export const mediaFolder = '/whatsMedia'
