ab929f2362b3f3fdc7701d1b48e2e61f505d9eeb
jefaokpta <jefaokpta@hotmail.com>
2021-11-10 10:01:53 -0300

    versao 1.4.3 rodando em homolog

--- src/static/staticVars.js
+++ src/static/staticVars.js
@@ -2,6 +2,6 @@
 export const urlBase = 'http://127.0.0.1:8080'
 export const mediaFolder = 'whatsMedia'
 
-// export const urlBase = 'http://128.0.0.1:9090'
-// export const urlBase = 'http://128.0.0.1:8080'
+// export const urlBase = 'http://128.0.0.1:9090' // HOMOLOGATION
+// export const urlBase = 'http://128.0.0.1:8080' // PRODUCTION
 // export const mediaFolder = '/whatsMedia'
